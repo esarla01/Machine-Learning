b1079e2578d528ed36c004f835a5e2a677b891ec
Erin Sarlak <114824028+esarla01@users.noreply.github.com>
2023-12-11 13:57:39 -0500

    Add files via upload

---
+++ HW2_ML/binary_metrics.py
@@ -0,0 +1,207 @@
+'''
+calc_binary_metrics
+# 
+Provides implementation of common metrics for assessing a binary classifier's
+hard decisions against true binary labels, including:
+* accuracy
+* true positive rate and true negative rate (TPR and TNR)
+* positive predictive value and negative predictive value (PPV and NPV)
+'''
+
+import numpy as np
+
+
+def calc_TP_TN_FP_FN(ytrue_N, yhat_N):
+    ''' Count the four possible states of true and predicted binary values.
+
+    Args
+    ----
+    ytrue_N : 1D array, shape (n_examples,) = (N,)
+        All values must be either 0 or 1. Will be cast to int dtype.
+        Each entry represents the binary 'true' label of one example
+        One entry per example in current dataset
+    yhat_N : 1D array, shape (n_examples,) = (N,)
+        All values must be either 0 or 1. Will be cast to int dtype.
+        Each entry represents a predicted label for one example
+        One entry per example in current dataset.
+        Needs to be same size as ytrue_N.
+
+    Returns
+    -------
+    TP : int
+        Number of true positives
+    TN : int
+        Number of true negatives
+    FP : int
+        Number of false positives
+    FN : int
+        Number of false negatives
+
+    Examples
+    --------
+    >>> N = 8
+    >>> ytrue_N = np.asarray([0., 0., 0., 0., 1., 1., 1., 1.])
+    >>> yhat_N  = np.asarray([0., 0., 1., 0., 1., 1., 0., 0.])
+    >>> TP, TN, FP, FN = calc_TP_TN_FP_FN(ytrue_N, yhat_N)
+    >>> TP
+    2
+    >>> TN
+    3
+    >>> FP
+    1
+    >>> FN
+    2
+    >>> np.allclose(TP + TN + FP + FN, N)
+    True
+    '''
+    # Cast input to integer just to be sure we're getting what's expected
+    ytrue_N = np.asarray(ytrue_N, dtype=np.int32)
+    yhat_N = np.asarray(yhat_N, dtype=np.int32)
+
+    assert (ytrue_N.shape == yhat_N.shape)
+
+    n_examples = ytrue_N.shape[0]
+
+    # Calculate the number of true pos, true neg, false pos, and true neg
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    for x in range(n_examples):
+        if ((ytrue_N[x] == 1) and (yhat_N[x] == 1)):
+            TP += 1
+        elif ((ytrue_N[x] == 0) and (yhat_N[x] == 0)):
+            TN += 1
+        elif ((ytrue_N[x] == 0) and (yhat_N[x] == 1)):
+            FP += 1
+        elif ((ytrue_N[x] == 1) and (yhat_N[x] == 0)):
+            FN += 1
+
+    return TP, TN, FP, FN
+
+
+def calc_ACC(ytrue_N, yhat_N):
+    ''' Compute the accuracy of provided predicted binary values.
+
+    Args
+    ----
+    ytrue_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents the binary 'true' label of one example
+        One entry per example in current dataset
+    yhat_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents a predicted label for one example
+        One entry per example in current dataset.
+        Needs to be same size as ytrue_N.
+
+    Returns
+    -------
+    acc : float
+        Accuracy = ratio of number correct over total number of examples
+
+    Examples
+    --------
+    >>> N = 8
+    >>> ytrue_N = np.asarray([0., 0., 0., 0., 1., 1., 1., 1.])
+    >>> yhat_N  = np.asarray([0., 0., 1., 0., 1., 1., 0., 0.])
+    >>> acc = calc_ACC(ytrue_N, yhat_N)
+    >>> print("%.3f" % acc)
+    0.625
+    '''
+    # Compute accuracy
+    TP, TN, FP, FN = calc_TP_TN_FP_FN(ytrue_N, yhat_N)
+    numerator = TP + TN
+    denominator = TP + TN + FP + FN + 1e-10
+    met_acc = numerator / denominator
+
+    return met_acc
+
+
+def calc_TPR(ytrue_N, yhat_N):
+    ''' Compute the true positive rate of provided predicted binary values.
+
+    Also known as the recall.
+
+    Args
+    ----
+    ytrue_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents the binary 'true' label of one example
+        One entry per example in current dataset
+    yhat_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents a predicted label for one example
+        One entry per example in current dataset.
+        Needs to be same size as ytrue_N.
+
+    Returns
+    -------
+    tpr : float
+        TPR = ratio of true positives over total labeled positive
+
+    Examples
+    --------
+    >>> N = 8
+    >>> ytrue_N = np.asarray([0., 0., 0., 0., 1., 1., 1., 1.])
+    >>> yhat_N  = np.asarray([0., 0., 1., 0., 1., 1., 0., 0.])
+    >>> tpr = calc_TPR(ytrue_N, yhat_N)
+    >>> print("%.3f" % tpr)
+    0.500
+
+    # Verify what happens with empty input
+    >>> empty_val = calc_TPR([], [])
+    >>> print("%.3f" % empty_val)
+    0.000
+    '''
+    # Compute TPR
+    TP, TN, FP, FN = calc_TP_TN_FP_FN(ytrue_N, yhat_N)
+    numerator = TP
+    denominator = TP + FN + 1e-10
+    TPR = numerator / denominator
+    return TPR
+
+
+def calc_PPV(ytrue_N, yhat_N):
+    ''' Compute positive predictive value of provided predicted binary values.
+
+    Also known as the precision.
+
+    Args
+    ----
+    ytrue_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents the binary 'true' label of one example
+        One entry per example in current dataset
+    yhat_N : 1D array of floats, shape (n_examples,) = (N,)
+        All values must be either 0.0 or 1.0.
+        Each entry represents a predicted label for one example
+        One entry per example in current dataset.
+        Needs to be same size as ytrue_N.
+
+    Returns
+    -------
+    ppv : float
+        PPV = ratio of true positives over total predicted positive.
+
+    Examples
+    --------
+    >>> N = 8
+    >>> ytrue_N = np.asarray([0., 0., 0., 0., 1., 1., 1., 1.])
+    >>> yhat_N  = np.asarray([0., 0., 1., 0., 1., 1., 0., 0.])
+    >>> ppv = calc_PPV(ytrue_N, yhat_N)
+    >>> print("%.3f" % ppv)
+    0.667
+
+    # Verify what happens with empty input
+    >>> empty_val = calc_PPV([], [])
+    >>> print("%.3f" % empty_val)
+    0.000
+    '''
+    # Compute PPV
+    TP, TN, FP, FN = calc_TP_TN_FP_FN(ytrue_N, yhat_N)
+    numerator = TP
+    denominator = TP + FP + 1e-10
+    PPV = numerator / denominator
+    return PPV
